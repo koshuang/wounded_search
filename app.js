1cc8570700506f1c47bb85c19dfb3d998f8a8315
Kos <koshuang@gmail.com>
2015-06-28 14:43:26 +0800

    add fussy search

--- app.js
+++ app.js
@@ -3,10 +3,45 @@ angular.module('app', ['ngResource']);
 angular.module('app')
   .controller('HomeController', function(UserService) {
     var vm = this;
+    var fuse;
 
-    UserService.getUsers().then(function(response) {
-      vm.users = response.data;
-    });
+    vm.search = search;
+
+    activate();
+
+    function activate() {
+      fetchUsers();
+    }
+
+    function fetchUsers() {
+      UserService.getUsers().then(function(response) {
+        vm.users = response.data;
+        vm.result = response.data;
+
+        fuse = new Fuse(vm.users, {
+          keys: ['編號', '縣市別', '收治單位', '檢傷編號', '姓名', '性別', '國籍', '年齡',
+            '醫療檢傷', '救護檢傷', '即時動向', '轉診要求', '刪除註記'
+          ]
+        });
+      });
+    }
+
+    function search(name) {
+      name = name || vm.searchText;
+
+      if (!name) {
+        vm.result = vm.users;
+        return;
+      }
+
+      vm.result = fuse.search(name);
+    }
+
+  })
+  .filter('userFilter', function() {
+    return function(input) {
+      return input ? '\u2713' : '\u2718';
+    };
   })
   .factory('UserService', function($http) {
     return {
