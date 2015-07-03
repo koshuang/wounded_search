e45d7e955875c806b2e3b77579c88f67663a60d2
Kos Huang <kos.huang@iscreen.com>
2015-07-03 13:59:13 +0800

    add info to sidenav

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -84,6 +84,23 @@ angular.module('app')
       }
     };
   })
+  .directive('menuContent', function() {
+    return {
+      scope: {
+        section: '='
+      },
+      templateUrl: 'src/app/menu-content.html',
+      link: function($scope, $element) {
+        var controller = $element.parent().controller();
+
+        $scope.focusSection = function() {
+          // set flag to be used later when
+          // $locationChangeSuccess calls openPage()
+          controller.autoFocusContent = true;
+        };
+      }
+    };
+  })
   .factory('UserService', function($http) {
     var cache;
     return {
@@ -119,23 +136,27 @@ angular.module('app')
   .factory('menu', [
     '$location',
     '$rootScope',
-    function($location) {
+    function($location, $rootScope) {
 
       var sections = [];
 
+      //   社會局社工科電話 <a href="tel:02-27206528">02-27206528</a><br><br>
+      //   <a target="_blank" href="https://tw.news.yahoo.com/%E5%B9%AB%E5%8A%A9%E5%85%AB%E4%BB%99%E5%82%B7%E8%80%85-%E4%BD%A0%E5%8F%AF%E4%BB%A5%E5%81%9A%E5%BE%97%E6%9B%B4%E5%A4%9A-034650399.html">幫助八仙傷者 你可以做得更多</a><br><br>
+      //   <a href="http://er.mohw.g0v.tw/#/dashboard/file/default.json">全國重度級急救責任醫院急診即時訊息</a>
+      //   <small>資料來源：<a ng-href="{{vm.source.url}}" target="_blank">{{vm.source.name}}</a></small>
+      //   <small>最後更新時間：{{vm.lastmodify}}</small>
       sections.push({
-        name: '最新消息',
+        name: '資訊',
         type: 'toggle',
         pages: [{
-          name: 'IPAs',
-          type: 'toggle',
-          icon: '',
-          pages: [{
-            name: 'IPAs',
-            type: 'toggle',
-            state: '',
-            icon: ''
-          }]
+          name: '社會局社工科電話',
+          content: '02-27206528',
+          icon: 'fa fa-group',
+          type: 'content',
+        }, {
+          name: '最後更新時間',
+          content: $rootScope.lastmodify,
+          type: 'content',
         }]
       });
 
