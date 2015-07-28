722d146696df1b0096b20eba4876a0cb16c39971
Kos <koshuang@gmail.com>
2015-07-29 00:04:18 +0800

    use gulp-browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,41 @@
 var gulp = require('gulp');
-var webserver = require('gulp-webserver');
+var inject = require('gulp-inject');
+var browserSync = require('browser-sync').create();
 
-gulp.task('serve', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      livereload: {
-        enable: true,
-        port: 4321
-      },
-      port: 4000,
-      fallback: 'index.html',
-      open: true
-    }));
+gulp.task('watch', ['inject'], function() {
+
+  gulp.watch(['/*.html', 'bower.json'], ['inject']);
+
+  gulp.watch([
+    './src/styles/**/*.css'
+  ], function(event) {
+    gulp.start('inject');
+  });
+
+  gulp.watch('./src/**/*.js', function(event) {
+    gulp.start('inject');
+  });
+
+  gulp.watch('./**/*.html', function(event) {
+    browserSync.reload(event.path);
+  });
+});
+
+gulp.task('inject', function() {
+  var target = gulp.src('./index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['./src/**/*.js', './src/**/*.css'], {
+    read: false
+  });
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('serve', ['watch'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 });
