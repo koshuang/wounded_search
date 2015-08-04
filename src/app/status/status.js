8dae5fa73991f9d2ca94903ec5c63ba44de16ac1
Kos <koshuang@gmail.com>
2015-08-04 08:53:21 +0800

    refactoring

--- src/app/status/status.js
+++ src/app/status/status.js
@@ -56,10 +56,10 @@ angular.module('app')
         groupByHospitalName('收治單位'));
       var hospitalUsers = getHospitals(vm.users);
 
-      vm.hospitals = R.map(function(h) {
+      vm.hospitals = R.map(function(hospital) {
         return {
-          key: h.name,
-          value: h.users.length
+          key: hospital.name,
+          value: hospital.users.length
         };
       }, hospitalUsers);
 
@@ -72,10 +72,10 @@ angular.module('app')
 
       vm.injuries = doit(vm.users);
 
-      vm.injuries = R.map(function(h) {
+      vm.injuries = R.map(function(hospital) {
         return {
-          key: h.name || '其他',
-          value: h.users.length
+          key: hospital.name || '其他',
+          value: hospital.users.length
         };
       }, vm.injuries);
     }
@@ -86,17 +86,17 @@ angular.module('app')
 
       vm.formalInjuries = doit(vm.users);
 
-      vm.formalInjuries = R.map(function(h) {
+      vm.formalInjuries = R.map(function(hospital) {
         return {
-          key: h.name || '其他',
-          value: h.users.length
+          key: hospital.name || '其他',
+          value: hospital.users.length
         };
       }, vm.formalInjuries);
     }
 
     function statusStatistic() {
-      var groupByHospitalName = R.groupBy(function(u) {
-        var status = u['即時動向'];
+      var groupByHospitalName = R.groupBy(function(user) {
+        var status = user['即時動向'];
 
         if (status.indexOf('自動出院') !== -1) {
           return '出院';
@@ -109,10 +109,10 @@ angular.module('app')
 
       vm.allStatus = doit(vm.users);
 
-      vm.allStatus = R.map(function(h) {
+      vm.allStatus = R.map(function(hospital) {
         return {
-          key: h.name || '',
-          value: h.users.length
+          key: hospital.name || '',
+          value: hospital.users.length
         };
       }, vm.allStatus);
     }
